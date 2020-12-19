a49b789036cff64850c0d3093c0cc4eae22fbd32
aaronthebaron77 <70720711+aaronthebaron77@users.noreply.github.com>
2020-12-18 20:30:14 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -186,7 +186,7 @@ module.exports.canSee = (serv, chn, usr) => {
     const user = channel.members.find(m => m.user.username === usr);
     //console.log(user)
     if(user) return true;
-    else if(user.hasPermission(READ_MESSAGE_HISTORY)) return false;
+    else if(user && !user.hasPermission(READ_MESSAGE_HISTORY)) return false;
     else return false;
   } catch(e) {console.log(e); return false;}
 }
