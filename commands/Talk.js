f33dd55da6fb2307d01352fbc07223272bd87b64
aaronthebaron77 <70720711+aaronthebaron77@users.noreply.github.com>
2021-01-26 20:16:01 -0600

    Update Talk.js

--- commands/Talk.js
+++ commands/Talk.js
@@ -33,10 +33,10 @@ module.exports.run = async (bot, message, args) => {
         
         let response = await message.channel.send("Please Wait...");
         
-        const responseText = (await page.$('#gtext')).getProperty('textContent');
+        const responseText = await (await page.$('#gtext')).getProperty('textContent');
         
         async function monitorResponse () {
-          const newVal = (await page.$('#gtext')).getProperty('textContent');
+          const newVal = await (await page.$('#gtext')).getProperty('textContent');
           console.log(responseText); console.log(newVal);
           if (newVal !== responseText) {
             await response.edit(newVal);
